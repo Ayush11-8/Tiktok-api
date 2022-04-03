e835a6ffe5c2c1ff4970193b74c7aeb6f2ecc75c
Ayush Goswami <ayush.goswami@nmbrsystems.com>
2022-04-03 14:25:50 +0530

    Jar file added with java 8 jdk

---
+++ src/test/java/com/example/Tiktokapi/TiktokApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tiktokapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
